85aa4ce6acbf67f34b04453a6faa9067e3e1d614
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-09-13 00:41:34 +0300

    Log in, Log Out and Database System Implement

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_uploads import IMAGES, UploadSet, configure_uploads
 from forms import PatientDetails, ImageUpload, HospitalRegister, LogIn
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
-# from skimage import color
-# from skimage import io
-# import pickle
+from sqlalchemy.exc import IntegrityError
 import os
 
 photos = UploadSet("photos", IMAGES)
@@ -15,23 +15,54 @@ photos = UploadSet("photos", IMAGES)
 app = Flask(__name__)
 app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
 app.config["SECRET_KEY"] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tb-hospital-data.db'
+db = SQLAlchemy(app)
 configure_uploads(app, photos)
 Bootstrap(app)
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hospital = db.Column(db.String(500), unique=True, nullable=False)
+    code = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
+
+db.create_all()
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = HospitalRegister()
     if form.validate_on_submit():
-        pass
+        new_user = User(
+            hospital=form.name.data,
+            code=form.code.data,
+            password=generate_password_hash(form.password.data, method="pbkdf2:sha256", salt_length=8)
+        )
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            pass
+        login_user(new_user)
+        return redirect(url_for('get_data'))
     return render_template("register.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LogIn()
     if form.validate_on_submit():
-        pass
+        user_available = User.query.filter_by(code=form.code.data).first()
+        if user_available and check_password_hash(user_available.password, form.password.data):
+            login_user(user_available)
+            return redirect(url_for('get_data'))
+        else:
+            flash("Invalid Credentials")
+            return redirect(url_for('login'))
     return render_template("login.html", form=form)
 
 
